e3853a2fa0588c6540649228845e9ec3c46a26aa
Maeva <merour.maeva@gmail.com>
2025-04-13 16:35:02 +0200

    changement de paramètres dans les () et retour de réponse false or true

--- assets/js/palindrome/palindrome.js
+++ assets/js/palindrome/palindrome.js
@@ -1,15 +1,16 @@
-function estPalindrome(mot){
-    let mot = String(prompt("mot"))
-    let motInverse = mot.split(""),reverse(""), join("")                    // découpe, inverse et ré-assemble les caractère pour obtenir le mot renversé
+function estPalindrome(){                                                   // ici pas besoin de renseigner de paramètres donc je peux laisser les () vides ! car ma valeur qui serait mot est à déterminer par le prompt
+    let mot = String(prompt("S'agit-il d'un palindrome ?"))                 // Quand ta fonction n’a pas besoin d’infos externes, tu ne mets rien dans les parenthèses. C’est un peu comme dire :
+                                                                            // "Je vais faire ce que j’ai à faire tout seul, sans que tu me donnes quoi que ce soit."
+    let motInverse = mot.split("").reverse().join("")                    // découpe, inverse et ré-assemble les caractère pour obtenir le mot renversé
 
     if (mot === motInverse ){
-        mot = true
         alert("Votre mot est un palindrome !")
+        return true
     } else {
-        mot = false
         alert("Essaye encore !")
+        return false
     }
 }
 
-console.log(estPalindrome("radar"))
-console.log(estPalindrome("voiture"));
+let reponse = estPalindrome()
+console.log(reponse)

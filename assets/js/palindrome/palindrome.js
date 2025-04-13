65c65013b41e8bc2eff43f45e53d1434f36169df
Maeva <merour.maeva@gmail.com>
2025-04-13 16:39:41 +0200

    avec le bon lien dans le html ça marche mieux

--- assets/js/palindrome/palindrome.js
+++ assets/js/palindrome/palindrome.js
@@ -1,4 +1,5 @@
 function estPalindrome(){                                                   // ici pas besoin de renseigner de paramètres donc je peux laisser les () vides ! car ma valeur qui serait mot est à déterminer par le prompt
+    do{
     let mot = String(prompt("S'agit-il d'un palindrome ?"))                 // Quand ta fonction n’a pas besoin d’infos externes, tu ne mets rien dans les parenthèses. C’est un peu comme dire :
                                                                             // "Je vais faire ce que j’ai à faire tout seul, sans que tu me donnes quoi que ce soit."
     let motInverse = mot.split("").reverse().join("")                    // découpe, inverse et ré-assemble les caractère pour obtenir le mot renversé
@@ -11,6 +12,7 @@ function estPalindrome(){                                                   // i
         return false
     }
 }
+} while (mot === false)
 
 let reponse = estPalindrome()
 console.log(reponse)

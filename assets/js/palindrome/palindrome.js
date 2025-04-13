9bf9f152b4b42e16a2301748b9bc54f0de66de01
Maeva <merour.maeva@gmail.com>
2025-04-13 16:21:30 +0200

    avant test de fonction

--- assets/js/palindrome/palindrome.js
+++ assets/js/palindrome/palindrome.js
@@ -1,10 +1,13 @@
 function estPalindrome(mot){
     let mot = String(prompt("mot"))
+    let motInverse = mot.split(""),reverse(""), join("")                    // découpe, inverse et ré-assemble les caractère pour obtenir le mot renversé
 
-    if (mot === ){
+    if (mot === motInverse ){
         mot = true
+        alert("Votre mot est un palindrome !")
     } else {
         mot = false
+        alert("Essaye encore !")
     }
 }
 

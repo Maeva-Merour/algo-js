2ebfc5dba6c7a903205457213889e3dd4578f797
Maeva <merour.maeva@gmail.com>
2025-04-13 16:50:56 +0200

    passage au while plus adapté que do...while - déplacement devariable avant while et introduction de réponseValide=false / mise en boucle de la fonction

--- assets/js/palindrome/palindrome.js
+++ assets/js/palindrome/palindrome.js
@@ -1,18 +1,22 @@
-function estPalindrome(){                                                   // ici pas besoin de renseigner de paramètres donc je peux laisser les () vides ! car ma valeur qui serait mot est à déterminer par le prompt
-    do{
-    let mot = String(prompt("S'agit-il d'un palindrome ?"))                 // Quand ta fonction n’a pas besoin d’infos externes, tu ne mets rien dans les parenthèses. C’est un peu comme dire :
+function estPalindrome(){                                               // ici pas besoin de renseigner de paramètres donc je peux laisser les () vides ! car ma valeur qui serait mot est à déterminer par le prompt
+    let reponseValide = false
+    let mot
+                                                      
+    while (true) {
+    mot = String(prompt("S'agit-il d'un palindrome ?"))                 // Quand ta fonction n’a pas besoin d’infos externes, tu ne mets rien dans les parenthèses. C’est un peu comme dire :
                                                                             // "Je vais faire ce que j’ai à faire tout seul, sans que tu me donnes quoi que ce soit."
     let motInverse = mot.split("").reverse().join("")                    // découpe, inverse et ré-assemble les caractère pour obtenir le mot renversé
 
     if (mot === motInverse ){
         alert("Votre mot est un palindrome !")
-        return true
-    } else {
+        reponseValide = true
+        // return true
+
+    } else {                                                            // on enlève le return false pour éviter de quitter la boucle tant que l'on a pas un palindrome
         alert("Essaye encore !")
-        return false
     }
 }
-} while (mot === false)
+}
 
 let reponse = estPalindrome()
 console.log(reponse)

53f4ec15f39df46fca891523374a2432cee9f35c
Maeva <merour.maeva@gmail.com>
2025-04-13 16:02:25 +0200

    bulle à tri fini et testé

--- assets/js/tri-a-bulle/script-bulle.js
+++ assets/js/tri-a-bulle/script-bulle.js
@@ -1,19 +1,28 @@
 function triABulles(nombres) {
+
     for (let i = 0; i < arrayBubble.length; i++) {
         console.log(arrayBubble[i])
+
+        arrayBubble.forEach((valeur, index, arr) => {
+                                                                                // valeur : valeur de l'élément actuel du tableau qui est en train d'être traité. index : C'est l'indice (la position) de l'élément actuel dans le tableau. arr :  C'est une référence au tableau original sur lequel la méthode forEach est appelée
+            if (index < arrayBubble.length - i - 1) {                           //  Ne comparer que jusqu'à l'avant-dernier élément non trié
+                                                                                  
+                    if (arr[index] > arr[index + 1]){                            //  Si l'élément actuel est plus grand que le suivant, les échanger
+
+                        let arrayGhost = arr[index];                             // Échanger les éléments
+                        arr[index] = arr[index + 1];
+                        arr[index + 1] = arrayGhost;
+                    }
+                }
+        })
     }
-
-    arrayBubble.forEach((valeur, index) => {                                 // valeur : C'est la valeur de l'élément actuel du tableau qui est en train d'être traité.
-        if (index < arrayBubble.length - i - 1) {                           // index : C'est l'indice (la position) de l'élément actuel dans le tableau. Le premier élément a l'indice 0, le deuxième l'indice 1, etc.
-                                                                            // arr : C'est une référence au tableau original sur lequel la méthode forEach est appelée.
-
-        } else if (index > arrayBubble.lenght - i + 1) {
-
-
-        }
-    })
+    return arrayBubble
 }
 
 let arrayBubble = [56, 89, 57, 20.4, 114, 87, 95.7, 30, 74]
 triABulles(arrayBubble)
-// console.log(arrayBubble)
+console.log(arrayBubble)
+
+
+// Je ne te cahce pas m'être aidée de l'IA dans la compréhension du résultat.
+// J'ai un peu de mal à comprendre arr

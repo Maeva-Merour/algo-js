5f1ba22a95bd3b62c3600434ce51b67452437166
Maeva <merour.maeva@gmail.com>
2025-04-13 16:17:43 +0200

    palindrome debut

---
+++ assets/js/palindrome/palindrome.js
@@ -0,0 +1,12 @@
+function estPalindrome(mot){
+    let mot = String(prompt("mot"))
+
+    if (mot === ){
+        mot = true
+    } else {
+        mot = false
+    }
+}
+
+console.log(estPalindrome("radar"))
+console.log(estPalindrome("voiture"));
--- assets/js/tri-a-bulle/script-bulle.js
+++ assets/js/tri-a-bulle/script-bulle.js
@@ -7,9 +7,9 @@ function triABulles(nombres) {
                                                                                 // valeur : valeur de l'élément actuel du tableau qui est en train d'être traité. index : C'est l'indice (la position) de l'élément actuel dans le tableau. arr :  C'est une référence au tableau original sur lequel la méthode forEach est appelée
             if (index < arrayBubble.length - i - 1) {                           //  Ne comparer que jusqu'à l'avant-dernier élément non trié
                                                                                   
-                    if (arr[index] > arr[index + 1]){                            //  Si l'élément actuel est plus grand que le suivant, les échanger
+                    if (arr[index] > arr[index + 1]){                            // Si l'élément actuel est plus grand que le suivant, 
 
-                        let arrayGhost = arr[index];                             // Échanger les éléments
+                        let arrayGhost = arr[index];                             // Échanger les éléments. arr me permet d'accéder à l'ensemble du tableau et d'en modifier directement les éléments pendant que je le parcours.
                         arr[index] = arr[index + 1];
                         arr[index + 1] = arrayGhost;
                     }
@@ -24,5 +24,5 @@ triABulles(arrayBubble)
 console.log(arrayBubble)
 
 
-// Je ne te cahce pas m'être aidée de l'IA dans la compréhension du résultat.
-// J'ai un peu de mal à comprendre arr
+// Je ne te cache pas m'être aidée de l'IA dans la compréhension du résultat pour la partie qui concerne l'échange de valeurs.
+// J'ai eu un peu de mal à comprendre arr et jusqu'à l'étape où il est utilisé j'étais en bonne voie de code "toute seule".
